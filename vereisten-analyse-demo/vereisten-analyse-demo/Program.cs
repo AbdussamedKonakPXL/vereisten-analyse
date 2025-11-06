45d270e7fdad0f33504f73a1f6360f143b1062dd
Samed Konak <12400611@student.pxl.be>
2025-11-06 13:39:22 +0100

    DEMO-04

--- vereisten-analyse-demo/vereisten-analyse-demo/Program.cs
+++ vereisten-analyse-demo/vereisten-analyse-demo/Program.cs
@@ -7,20 +7,32 @@
             string juisteNaam = "SamedPXL";
             string juisteWachwood = "SamedPXL1";
 
-            Console.Write("Gebruikersnaam: ");
-            string naam = Console.ReadLine();
-
-            Console.Write("Wachtwoord: ");
-            string wachtwoord = Console.ReadLine();
-
-            if (naam == juisteNaam && wachtwoord == juisteWachwood)
+            int tries = 3; 
+            do
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Jij bent ingelogd!");
-                Console.ResetColor();
-            }
+                Console.Write("Gebruikersnaam: ");
+                string naam = Console.ReadLine();
 
-            
+                Console.Write("Wachtwoord: ");
+                string wachtwoord = Console.ReadLine();
+
+                if (naam == juisteNaam && wachtwoord == juisteWachwood)
+                {
+                    return;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Ingevulde gegevens zijn fout jij hebt nog {tries} pogingen");
+                    Console.ResetColor();
+                    tries -= 1;
+                }
+                
+            } while (tries != 0);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Te veel pogingen probeer het later opnieuw!!");
+            Console.ResetColor();
+
         }
     }
 }
