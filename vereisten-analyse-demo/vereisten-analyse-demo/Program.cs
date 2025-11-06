c639485af97a3e356a351f1d1c5747de27852c6d
Samed Konak <12400611@student.pxl.be>
2025-11-06 13:29:30 +0100

    DEMO -03

--- vereisten-analyse-demo/vereisten-analyse-demo/Program.cs
+++ vereisten-analyse-demo/vereisten-analyse-demo/Program.cs
@@ -4,14 +4,23 @@
     {
         static void Main(string[] args)
         {
+            string juisteNaam = "SamedPXL";
+            string juisteWachwood = "SamedPXL1";
+
             Console.Write("Gebruikersnaam: ");
             string naam = Console.ReadLine();
 
             Console.Write("Wachtwoord: ");
             string wachtwoord = Console.ReadLine();
 
+            if (naam == juisteNaam && wachtwoord == juisteWachwood)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Jij bent ingelogd!");
+                Console.ResetColor();
+            }
 
-
+            
         }
     }
 }
